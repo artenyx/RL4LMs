7ba009f3d7510502ce0ff487124956a27f449b8a
Gerald White <geraldkwhite10@gmail.com>
2023-03-14 15:15:20 -0500

    adding slurm script dir and first script

--- slurm/run_dd_gpt2_ppo.sh
+++ slurm/run_dd_gpt2_ppo.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#python scripts/training/train_text_generation.py --config_path scripts/training/task_configs/dialog/gpt2_ppo_edited.yml > exp.txt &
-echo 'SCRIPT COMPLETE' > exp2.txt
+python scripts/training/train_text_generation.py --config_path scripts/training/task_configs/dialog/gpt2_ppo_edited.yml && echo 'SCRIPT COMPLETE'
