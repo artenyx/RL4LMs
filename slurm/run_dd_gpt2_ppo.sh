68b82a3fb5c7f1e7744cc49d8afbabe6cf0d0f03
Gerald White <geraldkwhite10@gmail.com>
2023-03-23 14:41:56 -0700

    editing slurm script

--- slurm/run_dd_gpt2_ppo.sh
+++ slurm/run_dd_gpt2_ppo.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo -n "ENTER EXP NAME: "
-read -r EXP_NAME
-
 WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py \
---experiment_name "$EXP_NAME" \
+--experiment_name "EXP1" \
 --log_to_wandb
