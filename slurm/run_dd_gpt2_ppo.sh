7dd7ec77fdfcd55b5dc2bfe6d6a3999256a8b853
Gerald White <geraldkwhite10@gmail.com>
2023-03-31 23:29:18 -0500

    testing policy separation in causal_policy.py

--- slurm/run_dd_gpt2_ppo.sh
+++ slurm/run_dd_gpt2_ppo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py \
---config_path scripts/training/task_configs/dialog/gpt2_ppo_edited.yml \
+--config_path scripts/training/task_configs/dialog/gpt2_ppo.yml \
 --experiment_name $1 \
 --base_path_to_store_results /share/data/kartik-collab/geraldkwhite/ \
 --log_to_wandb
