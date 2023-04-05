8d1ccadb0af40d9b20dca0e5fae7990306b83e72
Gerald White <geraldkwhite10@gmail.com>
2023-04-05 14:12:58 -0500

    large model + adding name_generation.py

--- slurm/run_dd_gpt2_ppo.sh
+++ slurm/run_dd_gpt2_ppo.sh
@@ -2,6 +2,5 @@
 
 WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py \
 --config_path scripts/training/task_configs/dialog/gpt2_ppo.yml \
---experiment_name $1 \
 --base_path_to_store_results /share/data/kartik-collab/geraldkwhite/ \
 --log_to_wandb
