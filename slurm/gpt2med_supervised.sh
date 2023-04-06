3628febad6c55485627b64f4cb4e397cbb696bd5
Gerald White <geraldkwhite10@gmail.com>
2023-04-06 15:08:28 -0500

    changing script

---
+++ slurm/gpt2med_supervised.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py \
+--config_path scripts/training/task_configs/dialog/gpt2_supervised_edited.yml \
+--base_path_to_store_results /share/data/kartik-collab/geraldkwhite/ \
+--log_to_wandb
