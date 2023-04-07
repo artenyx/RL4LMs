cb1d434ac5aaa4b03b18c8d85ffcd177cbdc4557
Gerald White <geraldkwhite10@gmail.com>
2023-04-07 12:03:12 -0500

    changing script

--- slurm/gpt2_ppo_singlerefsize.sh
+++ slurm/gpt2_ppo_singlerefsize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py \
---config_path scripts/training/task_configs/dialog/gpt2_ppo.yml \
+--config_path "scripts/training/task_configs/${1}/gpt2_ppo.yml" \
 --base_path_to_store_results /share/data/kartik-collab/geraldkwhite/ \
 --log_to_wandb \
---base_model_name "$1" \
---ref_model_name "$2"
+--base_model_name "$2" \
+--ref_model_name "$3"
