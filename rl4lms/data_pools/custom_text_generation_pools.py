9b1945bf4275cf93ae71fd9fe3868052622ebd9b
Gerald White <geraldkwhite10@gmail.com>
2023-04-21 14:00:41 -0400

    changing task name definition in train_text_generation.py

--- rl4lms/data_pools/custom_text_generation_pools.py
+++ rl4lms/data_pools/custom_text_generation_pools.py
@@ -449,7 +449,7 @@ class IWSLT2017EnDe(TextGenPool):
     @classmethod
     def get_dataset(cls, split: str):
         dataset = load_dataset(
-            "iwslt2017", "iwslt2017-de-en", ignore_verifications=True, streaming=True)
+            "iwslt2017", "iwslt2017-de-en", verification_mode="BASIC_CHECKS")
         return dataset[split]
 
     @classmethod
