27254c058587e0e2a1f35eabe17e7469a800c159
Gerald White <geraldkwhite10@gmail.com>
2023-04-24 12:30:42 -0400

    changing task name definition in train_text_generation.py

---
+++ slurm/single_exp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+task_name="$1"
+base_model_name="$2"
+ref_model_name="$3"
+experiment_name="$4"
+
+args=()
+if [[ "$base_model_name" == *gpt2* ]]; then
+  args+=("--config_path scripts/training/task_configs/${1}/gpt2_ppo.yml")
+elif [[ "$base_model_name" == *t5* ]]; then
+  args+=("--config_path scripts/training/task_configs/${1}/t5_ppo.yml")
+fi
+
+if [ -z "$4" ]; then
+  args+=("--experiment_name $experiment_name")
+fi
+
+args+=('--base_path_to_store_results /share/data/kartik-collab/geraldkwhite/' \
+'--log_to_wandb' \
+"--base_model_name $base_model_name" \
+"--ref_model_name $ref_model_name" \
+"--task_name $task_name")
+
+WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py "${args[@]}"
