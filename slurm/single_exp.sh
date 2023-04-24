dcdd225ec59eb968accd97893459138d0b5269e0
Gerald White <geraldkwhite10@gmail.com>
2023-04-24 13:28:30 -0400

    changing script protocol

--- slurm/single_exp.sh
+++ slurm/single_exp.sh
@@ -21,10 +21,10 @@ if [ -z "$5" ] || [ "$4" != NONE ]; then
   args+=("--gamma $gamma")
 fi
 
-args+=('--base_path_to_store_results /share/data/kartik-collab/geraldkwhite/' \
-'--log_to_wandb' \
+args+=("--base_path_to_store_results /share/data/kartik-collab/geraldkwhite/" \
+"--log_to_wandb" \
 "--base_model_name $base_model_name" \
 "--ref_model_name $ref_model_name" \
 "--task_name $task_name")
 
-WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py "${args[@]}"
+WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py ${args[@]}
