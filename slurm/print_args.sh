9902e57ab0708b929208ff35046f086bdbdd8b03
Gerald White <geraldkwhite10@gmail.com>
2023-05-01 15:05:27 -0500

    adding group variable

--- slurm/print_args.sh
+++ slurm/print_args.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "SINGLE EXP ARGS:"
-echo "1:task_name 2:base_model_name 3:ref_model_name 4:experiment_name 5:gamma"
+echo "1:task_name 2:base_model_name 3:ref_model_name 4:experiment_name 5:group_name"
 
 echo "FULL EXP ARGS:"
-echo "1:task_name 2:base_model_sm 3:run_sm_lg_exp 4:gamma"
+echo "1:task_name 2:base_model_sm 3:run_sm_lg_exp 4:group_name"
 
 echo "task_name: name of task in RL4LMs config"
 echo "base_model_name: name of hf base model"
@@ -13,3 +13,4 @@ echo "experiment_name: optional name of experiment if continuing (NONE if not us
 echo "gamma: discount factor"
 echo "base_model_sm: whether or not to use small base model (e.g. t5-small or distilgpt2)"
 echo "run_sm_lg_exp: whether to run experiment where base model is larger than reference model"
+echo "group_name: wandb init group name"
