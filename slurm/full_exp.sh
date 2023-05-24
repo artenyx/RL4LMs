884a9a64322c7d338153e190abce896996b618ff
Gerald White <geraldkwhite10@gmail.com>
2023-05-24 13:41:46 -0400

    changing common_gen yml

--- slurm/full_exp.sh
+++ slurm/full_exp.sh
@@ -42,7 +42,7 @@ fi
 
 for ref_model_name in ${ref_models[@]}
 do
-  sbatch -p speech-gpu -C 48g slurm/single_exp.sh "$task_name" "$base_model_name" "$ref_model_name" NONE "$group"
+  sbatch -p contrib-gpu-long -C 48g slurm/single_exp.sh "$task_name" "$base_model_name" "$ref_model_name" NONE "$group"
 done
 
 # speech-gpu
