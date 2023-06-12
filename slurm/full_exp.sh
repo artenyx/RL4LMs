66b22e980442d74d0bc0f372dabc1621254014dd
Gerald White <geraldkwhite10@gmail.com>
2023-06-12 09:33:22 -0500

    adding off_policy pipeline

--- slurm/full_exp.sh
+++ slurm/full_exp.sh
@@ -20,7 +20,7 @@ if [ -n "$5" ]; then
 else
   off_policy=NONE
 fi
-
+echo off_policy
 #setting base model
 if [[ "$task_name" == imdb_text_continuation ]] || [[ "$task_name" == dialog ]]; then
   if [[ $base_model_sm == true ]]; then
