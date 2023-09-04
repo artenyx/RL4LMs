7e6933fd4f1c3bb0a2a08011a2916dff6c290ef1
Gerald White <geraldkwhite10@gmail.com>
2023-09-04 19:32:27 -0400

    adding sweep functionality

--- slurm/single_exp.sh
+++ slurm/single_exp.sh
@@ -49,4 +49,4 @@ args+=("--base_path_to_store_results /share/data/kartik-collab/geraldkwhite/" \
 
 # shellcheck disable=SC2068
 WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py ${args[@]}
-# echo WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py ${args[@]} # for testing
+echo WANDB_API_KEY=92a8247f01352422f27fab17382f1f897dd4f745  python scripts/training/train_text_generation.py ${args[@]} # for testing
